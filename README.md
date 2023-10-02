722b9c680dce636a3d32f05d19f290b4df9e9fcf
asrii00 <117261755+asrii00@users.noreply.github.com>
2023-10-02 13:35:08 +0300

    adding javascript to contcact form

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# aslan-sandbox-saitti
-html höntsäilyt. 
+## HTML
+
+Website contains: 
+1. Basic HTML structure
+1. HTML elements like: headings, paragraphs, lists
+1. A HTML Form, links, and images (jpeg and GIF)
+1. A table (The Mondrian "painting" is a table)
+1. Semantic elements (header, nav, section, footer)
+
+
+## CSS
+
+Website contains:
+2. Basic styling (colors, fonts)
+2. Use of classes and ID's. 
+2. Responsive design elements (Mobile friendly)
+2. Use of flexbox (in navigation)
+2. A sincere attempt at having a clear and user-friendly layout. "Homepage straight from 2004" happens to be my preferred website aesthetic.
+
+
+## Javascript basics 
+
+Website contains: 
+3. -
+3. Multiple event listeners (blur, change) and basic DOM manipulations
+3. Use of functions - 
+
+## Asynchronous operations 
---
+++ contact.js
@@ -0,0 +1,91 @@
+// are you human choice doesn't get checked atm
+
+const marryCheckBox = document.getElementById('marry');
+const netWorthBox = document.getElementById('netWorthBox');
+const incomeBox = document.getElementById('incomeBox');
+
+
+function validateForm() {
+    let x = document.forms["form1"]["fname"].value;
+    let y = document.forms["form1"]["lname"].value;
+
+    if (x == "" || y == "") {
+        alert("Please include your full name.");
+        return false;
+    }
+    else {
+        let z = document.getElementById("email").value;
+        if (!z.includes('@') || !z.includes('.') || (z.length < 5)) {
+            alert("Please include a valid email.")
+            return false;
+        }
+        else {
+            let a = document.forms["form1"]["details"].value;
+            if (a.length < 15) {
+                alert("Please include your preferred date and location.")
+                return false;
+            }
+        }
+    }
+
+}
+
+marryCheckBox.addEventListener('change', (event) => { //if you go back, this doesn't get triggered
+    if (event.currentTarget.checked) {
+        document.getElementById("marryForMoney").style.display = "inline"
+
+    } else {
+        document.getElementById("marryForMoney").style.display = "none"
+
+    }
+})
+
+netWorthBox.addEventListener('blur', netWorthChecker);
+incomeBox.addEventListener('blur', incomeChecker); //bugs out sometimes? at least with autofill
+
+
+function netWorthChecker(){
+
+    
+    let x = document.getElementById("netWorthBox").value;
+    let text;
+    if (x == "") {
+        let x = document.getElementById("incomeGuide").style.display = "block";
+        text = "Please enter a value";
+    }
+    else if (isNaN(x)) {
+        let x = document.getElementById("networthGuide").style.display = "block";
+        text = "Please enter numbers only.";
+    } else if (x < 800000) {
+        text = "You are too poor. Please reconsider the intention to marry me.";
+    }
+    else {
+        text = "";
+    }
+    document.getElementById("networthGuide").innerHTML = text;
+
+}
+
+
+function incomeChecker(){
+
+    let x = document.getElementById("incomeBox").value;
+    let text;
+    if (x == "") {
+        let x = document.getElementById("incomeGuide").style.display = "block";
+        text = "Please enter a value";
+
+    }
+    else if (isNaN(x)) {
+        let x = document.getElementById("incomeGuide").style.display = "block";
+        text = "Please enter numbers only.";
+    } 
+    else if (x < 120000) {
+        text = "You don't make enough money. Please reconsider the intention to marry me.";
+    }
+    else {
+        text = "";
+    }
+    document.getElementById("incomeGuide").innerHTML = text;
+}
+
